6e28623bb275327d6a83837545da0a077f282ad0
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2026-08-02 13:43:11 -0500

    update about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,13 +19,9 @@ export default function About() {
       <section className={styles.sectionAbout}>
         <p className={styles.aboutText}>The Reason I Love Web Design</p>
 
-            <p>Growing up on a farm in a small-town community, I valued nature, family, community and above all learning about everything! In high school my counselor told me I would never be accepted into college.  For anyone that has faced those words, “you can’t”, the fire lit inside me. Since then, not only did I get into college, but I have graduated with not one but four educational degrees.</p> 
+            <p>Growing up on a farm in a small-town community, I valued nature, family, community and above all learning about everything! In high school my counselor told me I would never be accepted into college.  For anyone that has faced those words, “you can’t”, the fire lit inside me. Since then, not only did I get into college, but I have graduated with two major educational degrees.</p> 
             
-            <p>After earning my associates degree, I continued learning and became a Respiratory Therapist, completed a Bachelor of Business Administration degree, and finally my Master of Mass Communication degree with a concentration in Web Design</p>
-            
-            <p>Web design offers me a platform to combine all my learning experience, and the opportunity to continue learning because it is always evolving.  New design trends, tools, and technologies (like responsive design, accessibility standards, and UX/UI best practices) emerge frequently. This environment keeps me engaged and growing continuously. I am passionate about web design because it feeds my curiosity and rewards adaptability and exploration.</p>
-           
-            <p>Great web design isn’t just about aesthetics; it’s about understanding users and what they need, how they think, and how they interact with digital spaces. This is invaluable when translating goals into effective digital experiences. My diverse background helps me design for different audiences and helps me create inclusive and user-friendly website designs.</p>
+            <p>Web design offers me a platform to combine all my learning experience, and the opportunity to continue learning because it is always evolving. New design trends, tools, and technologies  emerge frequently. This environment keeps me engaged and growing continuously. I am passionate about web design because it feeds my curiosity and rewards adaptability and exploration.</p>
            
             <p>Take a look at my projects and see first hand the successes and challenges I have experienced. </p>
            
