28a1541b80a988cf0168b59f97d29ab1580b4625
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2026-08-02 13:53:01 -0500

    update contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -11,7 +11,7 @@ export default function Contact() {
       <section className={styles.sectionContact}>
         <h2>I'm looking for work!</h2>
         <h4 className={styles.contactMe}>If you'd like to get in touch with me: <br></br>
-        kspets@ufl.edu</h4>
+        kathy.spets@gmail.com</h4>
         <h4>You can also contact me here as well: {' '}
             <br></br>
             <a
@@ -22,13 +22,6 @@ export default function Contact() {
               LinkedIn
             </a>
             <br></br>
-            <a
-              href="https://github.com/kSpets25"
-              target="_blank"
-              rel="nonopener noreferrer"
-            >
-              Github
-            </a>
         </h4>
       </section>
     </main>
